7362ad52b21fbd873bce5d73157b10ce50182aa4
Val7498 <software@mike.ong>
2024-05-30 14:53:39 -0700

    replaced eval

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ app.config['PRINTER_ADDRESS'] = os.getenv("PRINTER_ADDRESS")
 
 @app.route('/api/version', methods=['GET'])
 def get_version():
+    print("Sending version information")
     # Mocked version information
-    return jsonify(api="0.1", text="OctoPrint 1.7.2")
+    return jsonify(api="0.1", text="OctoPrint 1.7.2"), 200
 
 @app.route('/api/files/local', methods=['POST'])
 def upload_file():
@@ -25,20 +26,21 @@ def upload_file():
         return jsonify(error="No selected file."), 400
     
     if file:
-        #Code taken from https://github.com/dabosch/Dremel-3D45-Undocumented/blob/master/Control_3D45_video.py
-        print("Sending to 3D45 Printer...")
-        print(file.filename)
+        #Code modified from https://github.com/dabosch/Dremel-3D45-Undocumented/blob/master/Control_3D45_video.py
         gcode = {'print_file': (file.filename, file.stream, "multipart/form-data")}
         try:
+            print("Sending %s to 3D45 Printer..." % file.filename)
             res = requests.post(app.config["PRINTER_ADDRESS"] + "/print_file_uploads", files=gcode, timeout=20.0)
-            if(eval(res.text)["error_code"] == 200):
+            if(res.status_code == 200):
+                print("Sent gcode to printer")
                 if request.form["print"] == "true":
-                    data = {'PRINT':file.filename}
-                    res2 = requests.post(app.config["PRINTER_ADDRESS"] + '/command', data=data,timeout=20.0)
-                    if(eval(res2.text)["error_code"] == 200):
-                        return jsonify(message="Printing..."), 200
+                    print("Sending command to print %s" % file.filename)
+                    res2 = requests.post(app.config["PRINTER_ADDRESS"] + '/command', data={'PRINT': file.filename},timeout=20.0)
+                    if(res2.status_code == 200):
+                        print("print job started")
+                        return jsonify(message="Print job started"), 200
                 else:
-                    return jsonify(message="Successfully uploaded file to 3D45 Printer")
+                    return jsonify(message="Successfully uploaded file to 3D45 Printer"), 200
             else:
                 return jsonify(error="Failed to upload file to the 3D45 Printer"), 500
         except:
